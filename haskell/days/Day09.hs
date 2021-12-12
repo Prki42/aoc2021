5f48fd2915407ce9223a4008bc561fca567a57bf
Aleksa Prtenjača <aleksa.prtenjaca03@gmail.com>
2021-12-12 15:40:36 +0100

    Day 9 - haskell

--- haskell/days/Day09.hs
+++ haskell/days/Day09.hs
@@ -3,47 +3,73 @@ module Main
     ) where
 
 import           Control.Arrow                  ( (&&&) )
-import           Data.Bifunctor                 ( Bifunctor(second) )
+import           Data.Bifunctor                 ( second )
 import           Data.Char                      ( digitToInt )
+import           Data.List                      ( sort
+                                                , sortOn
+                                                )
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as Map
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as Set
 
-data Cave = Cave
-    { grid   :: Map Int Int
-    , rowNum :: Int
-    , colNum :: Int
-    }
+type Cave = Map (Int, Int) Int
 type Input = Cave
 
-neighbours :: Int -> Int -> Int -> [Int]
-neighbours n rowN colN
-    | modCol > 0 && modCol < colN - 1 = (n + 1) : (n - 1) : upDown
-    | modCol > 0                      = (n - 1) : upDown
-    | otherwise                       = (n + 1) : upDown
-  where
-    upDown = filter (\n -> n >= 0 && n < rowN * colN) [n + colN, n - colN]
-    modCol = n `mod` colN
+colNum = 100
+rowNum = 100
 
+getNeighbours :: (Int, Int) -> [(Int, Int)]
+getNeighbours (x, y) = filter
+    (\(x, y) -> x < rowNum && x >= 0 && y < colNum && y >= 0)
+    allNeighbours
+    where allNeighbours = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
+search :: Map (Int, Int) Int -> (Int, Int) -> Set (Int, Int) -> Set (Int, Int)
+search m p s
+    | null neighboursNot9 = newS
+    | otherwise = foldl
+        (\c n -> if Set.member n c then c else Set.union c (search m n newS))
+        Set.empty
+        neighboursNot9
+  where
+    newS = Set.insert p s
+    neighboursNot9 =
+        filter (not . (`Set.member` s))
+            $ filter (\k -> m Map.! k /= 9)
+            $ getNeighbours p
 
 part1 :: Input -> Int
-part1 (Cave m rowN colN) = sum $ map (+ 1) a
-  where
-    a = map fst $ filter (\(val, ns) -> all (> val) ns) $ map
-        ( Data.Bifunctor.second (map (\n -> Map.findWithDefault 0 n m))
-        . (\(p, val) -> (val, neighbours p rowN colN))
-        )
-        t
-    t = Map.toList m
+part1 m =
+    sum
+        . map ((+ 1) . fst)
+        . filter (\(val, ns) -> all (> val) ns)
+        . map (second (map (m Map.!)))
+        . Map.foldrWithKey (\k x ks -> (x, getNeighbours k) : ks) []
+        $ m
 
 part2 :: Input -> Int
-part2 = const 2
+part2 m =
+    product
+        . take 3
+        . reverse
+        . sort
+        . map (Set.size . (\p -> search m p (Set.singleton p)))
+        $ pits
+  where
+    pits =
+        map fst
+            . filter (\(p, val) -> all ((> val) . (m Map.!)) (getNeighbours p))
+            . Map.toList
+            $ m
 
 prepare :: String -> Input
-prepare xs = Cave (Map.fromList $ zip [0 ..] g) (length rows) colN
-  where
-    rows = lines xs
-    colN = length . head $ rows
-    g    = concatMap (map digitToInt) rows
+prepare =
+    Map.fromList
+        . concatMap (\(i, arr) -> zipWith (\x y -> ((i, x), y)) [0 ..] arr)
+        . zip [0 ..]
+        . map (map digitToInt)
+        . lines
 
 
 main :: IO ()
